ee1ef2849d13376955e1230a910252160068da71
liliia-r <54743392+liliia-r@users.noreply.github.com>
2021-10-01 12:48:08 +0300

    3 more tasts add

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,40 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 export default {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(`${value}`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      typeof position !== "number" ||
+      position > this.chain.length ||
+      position < 1
+    ) {
+      this.chain = [];
+      throw new Error(`You can't remove incorrect link!`);
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let result = this.chain
+      .map((element) => `( ${element} )~~`)
+      .join("")
+      .slice(0, -2);
+    this.chain = [];
+    return result;
+  },
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,19 +1,52 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let transformedArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--discard-next") {
+      i += 2;
+      continue;
+    }
+    if (arr[i] === "--discard-prev") {
+      transformedArr.pop();
+      continue;
+    }
+    if (arr[i] === "--double-next") {
+      if (!arr[i + 1]) continue;
+      transformedArr.push(arr[i + 1]);
+      transformedArr.push(arr[i + 1]);
+      i += 1;
+      continue;
+    }
+    if (arr[i] === "--double-prev") {
+      if (!arr[i - 1]) continue;
+      if (transformedArr[transformedArr.length - 1] === arr[i - 1]) {
+        transformedArr.push(arr[i - 1]);
+      } else {
+        transformedArr.push(arr[i - 1]);
+        transformedArr.push(arr[i - 1]);
+      }
+      continue;
+    }
+    if (arr[i]) transformedArr.push(arr[i]);
+  }
+  return transformedArr;
 }
